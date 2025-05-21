94d54878510aae5bb1722ff6b7498be4476b4d85
Eduardo <juantokx@gmail.com>
2025-05-21 07:01:35 -0600

    Added types

---
+++ types/Integration.ts
@@ -0,0 +1,18 @@
+import { ObjectId } from "mongodb";
+
+export type IntegrationType = 'wa'
+
+export interface IntegrationDb extends Integration {
+    chatBotId: ObjectId,
+}
+
+export interface IntegrationRecord extends Integration {
+    _id: string,
+    chatBotId: string,
+}
+
+
+export interface Integration {
+    type: IntegrationType,
+    users: string[],
+}
