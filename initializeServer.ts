df0c09ab46c4e5baa313000e463e582af2423c13
Eduardo <juantokx@gmail.com>
2025-05-20 21:04:53 -0600

    Initial commit

---
+++ initializeServer.ts
@@ -0,0 +1,17 @@
+import { createBotInstance } from "./chatBot";
+import { getDatabase } from "./db";
+import { IntegrationDb } from "./types";
+
+export const initializeServer = async () => {
+  const db = await getDatabase();
+  const integrationsCollection = db.collection<IntegrationDb>("integrations");
+
+  const integrations = await integrationsCollection
+    .find({ type: "tg" })
+    .toArray();
+  integrations.forEach(async (integration) => {
+    const { chatBotId, config } = integration;
+    if (!config.token) return
+    await createBotInstance(config.token, chatBotId.toString())
+  });
+};
---
+++ types.ts
@@ -0,0 +1,20 @@
+import { ObjectId } from "mongodb"
+
+export type IntegrationType = 'wa' | 'tg'
+
+export interface IntegrationDb extends Integration {
+    chatBotId: ObjectId
+}
+
+export interface IntegrationRecord extends Integration {
+    _id: string,
+    chatBotId: string
+}
+
+export interface Integration {
+    type: IntegrationType,
+    users: string[],
+    config: {
+        [key: string]: string
+    }
+}
