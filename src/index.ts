4a02cf4f30e4fa481df4c205fce89e31ef3b3047
Eduardo <juantokx@gmail.com>
2025-05-21 07:08:07 -0600

    Moved all to src folder

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import dotenv from 'dotenv'
+import { getDatabase, connectToDatabase } from './db'
+import express, { RequestHandler } from 'express'
+import cors from 'cors'
+import morgan from 'morgan'
+import { initializeServer } from './initializeServer'
+
+dotenv.config()
+
+const PORT = process.env.PORT || 3000
+const app = express()
+
+app.disable('x-powered-by')
+app.use(cors()) // Primero permite CORS
+app.use(express.json()) // Luego maneja solicitudes JSON
+app.use(morgan('dev')) // Y por último el logging
+
+app.listen(PORT, async () => {
+	await connectToDatabase()
+	await initializeServer()
+	console.log(`Server running on port: ${PORT}`)
+})
---
+++ src/types/Chat.ts
@@ -0,0 +1,16 @@
+import { CoreMessage, Message } from 'ai'
+import { ObjectId } from 'mongodb'
+
+export interface ChatRecord extends Chat {
+	chatBotId: string
+}
+
+export interface ChatDb extends Chat {
+	chatBotId: ObjectId
+}
+
+export interface Chat {
+	messages: Message[]
+	name?: string
+	lastActive?: Date
+}
